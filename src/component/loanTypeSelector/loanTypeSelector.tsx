d1655519fe4f6d84cf9dbfd12051861832f5caa1
alaasassii <alaasassijs@gmail.com>
2023-10-01 10:11:21 +0100

    fetch products from json file

--- src/component/loanTypeSelector/loanTypeSelector.tsx
+++ src/component/loanTypeSelector/loanTypeSelector.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import './loanTypeSelector.scss'
+import { productsType } from '../../types/productsType'
+
+import axios, { AxiosResponse, AxiosError } from 'axios'
 const LoanTypeSelector = () => {
+    const [products, setProducts] = useState<productsType>([])
+    const [error, setError] = useState('')
+    useEffect(() => {
+        axios.get<productsType>('/products.json')
+            .then((response: AxiosResponse) => {
+                setProducts(response.data)
+            })
+            .catch((error: AxiosError) => {
+                setError(error.message)
+            })
+    })
+    if (error) return <h1>{error}</h1>
     return (
         <div className='loan__type__selector'>
 
