9667204f9493eabb6b278d6c059717144a7d4109
alaasassii <alaasassijs@gmail.com>
2023-10-01 10:13:49 +0100

    display the product

--- src/component/loanTypeSelector/loanTypeSelector.tsx
+++ src/component/loanTypeSelector/loanTypeSelector.tsx
@@ -18,7 +18,15 @@ const LoanTypeSelector = () => {
     if (error) return <h1>{error}</h1>
     return (
         <div className='loan__type__selector'>
-
+            {
+                products.map(
+                    (product, index) => (
+                        <div className={`product__${index}`}>
+                            <img src={product.image} alt={product.name} />
+                        </div>
+                    )
+                )
+            }
         </div>
     )
 }
